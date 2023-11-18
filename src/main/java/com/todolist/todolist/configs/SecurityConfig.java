53412ea26bb8b907ae33aeef09a1070f929cb2b9
Gabriel Cardoso Girarde <gabrielcardosogirarde@gmail.com>
2023-11-18 09:27:43 -0300

    add h2 memory

--- src/main/java/com/todolist/todolist/configs/SecurityConfig.java
+++ src/main/java/com/todolist/todolist/configs/SecurityConfig.java
@@ -1,14 +1,9 @@
 package com.todolist.todolist.configs;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,15 +13,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 import com.todolist.todolist.domain.filters.UsuarioAutenticacaoFiltro;
 import com.todolist.todolist.domain.filters.UsuarioAutorizacaoFiltro;
 import com.todolist.todolist.domain.utils.JwtUtils;
-import com.todolist.todolist.repositories.ListaRepository; 
+import com.todolist.todolist.repositories.ListaRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -46,11 +39,11 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http 
-            .cors().and().csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeHttpRequests()
+        return http
+                .cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/login", "/login/")
                 .permitAll()
                 .requestMatchers(HttpMethod.POST, "/login", "/login/**", "/usuarios")
@@ -60,24 +53,23 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .and()
                 .addFilter(new UsuarioAutenticacaoFiltro(authenticationManager(), jwtUtils))
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+                .build();
     }
-
 
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return config.getAuthenticationManager();
-    } 
+    }
 
     @Bean
     public PasswordEncoder passwordEnconder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-            .allowedOrigins("*");
-	}
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedOrigins("*");
+    }
 }
